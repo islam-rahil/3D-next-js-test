90b9cfd1b491d6a4c9de13cc2125d55ea092d8ee
islam-rahil <rahilmohamedislam@gmail.com>
2026-05-09 18:33:26 +0200

    Art

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Art from "../compoments/Art"
 export default function Home() {
   return (
     <main className="h-screen ">
-      <div className="h-full w-full grid-cols-3">
+      <div className="h-full w-full grid-cols-3 gap-10">
         <Art img="/tb1.jpg"/>
         <Art img="/tb2.jpg"/>
         <Art img="/tb3.jpg"/>
